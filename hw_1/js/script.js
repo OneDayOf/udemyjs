1edf8da5727090adb853107c67e1d4515ac057d8
Andrew <matrosow.andrew@yandex.ru>
2019-05-16 13:22:38 +0300

    Первое задание курса

---
+++ hw_1/js/script.js
@@ -0,0 +1,24 @@
+let money = prompt("Ваш бюджет на месяц?", "30000"),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "2019-05-16"),
+    appData = {
+        budget: money,
+        timeData: time,
+        expenses:{},
+        optionalExpenses: {},
+        income: {},
+        savings: false
+    }
+
+    q1 = prompt("Введите обязательную статью расходов в этом месяце", "Котлетки с пюрешкой"),
+    a1 = +prompt("Во сколько обойдется?", "150"),
+    q2 = prompt("Введите обязательную статью расходов в этом месяце", "Аренда квартиры"),
+    a2 = +prompt("Во сколько обойдется?", "15000");
+    
+    appData.expenses[q1] = a1;
+    appData.expenses[q2] = a2;
+    alert("Дневной бюджет: " + (money/30));
+
+    
+    
+    
+    
